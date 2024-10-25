3ce87fc99cb24300dd5f783859fa90e40b3f5bad
Gagan1505 <gagananand1505@gmail.com>
2024-10-25 10:04:34 +0530

    Assignment

--- dynamic-pdf-generator/src/main/java/com/example/dynamicpdfgenerator/PdfGeneratorApplication.java
+++ dynamic-pdf-generator/src/main/java/com/example/dynamicpdfgenerator/PdfGeneratorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class PdfGeneratorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(PdfGeneratorApplication.class, args);
 	}
 
 }
